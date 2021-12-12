47ccb487a472eecb2ddd17f7df3fa7c2dba6b8cb
rupaak <rupakt525@gmail.com>
2021-12-12 20:51:25 +0545

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Home());
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Ecommerce"),
+        ),
+      ),
+    );
+  }
+}
